859f6899007d26a953a61c5fd28af00554ea6385
amiryona1 <amiryona6@gmail.com>
2024-12-01 19:52:02 +0200

    Added detection colors using HSV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/CustomColorRange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/CustomColorRange.java
@@ -18,22 +18,45 @@ public class CustomColorRange
     // DEFAULT OPTIONS
     // -----------------------------------------------------------------------------
 
-    public static final CustomColorRange BLUE = new CustomColorRange(
-            ColorSpace.YCrCb,
-            new Scalar( 16,   0, 155),
-            new Scalar(255, 127, 255)
-    );
+//    public static final CustomColorRange BLUE = new CustomColorRange(
+//            ColorSpace.YCrCb,
+//            new Scalar( 16,   0, 155),
+//            new Scalar(255, 127, 255)
+//    );
+//
+//    public static final CustomColorRange RED = new CustomColorRange(
+//            ColorSpace.YCrCb,
+//            new Scalar( 32, 176,  0),
+//            new Scalar(255, 255, 132)
+//    );
+//
+//    public static final CustomColorRange YELLOW = new CustomColorRange(
+//            ColorSpace.YCrCb,
+//            new Scalar(32, 128, 0),
+//            new Scalar(255, 169, 100)
+//    );
+//
+//    public static final CustomColorRange GREEN = new CustomColorRange(
+//            ColorSpace.YCrCb,
+//            new Scalar( 32,   0,   0),
+//            new Scalar(255, 120, 133)
+//    );
+public static final CustomColorRange BLUE = new CustomColorRange(
+        ColorSpace.HSV,
+        new Scalar(100, 150, 50),
+        new Scalar(140, 255, 255)
+);
 
     public static final CustomColorRange RED = new CustomColorRange(
-            ColorSpace.YCrCb,
-            new Scalar( 32, 176,  0),
-            new Scalar(255, 255, 132)
+            ColorSpace.HSV,
+            new Scalar(0, 76, 98),
+            new Scalar(0, 99, 67)
     );
 
     public static final CustomColorRange YELLOW = new CustomColorRange(
-            ColorSpace.YCrCb,
-            new Scalar(32, 128, 0),
-            new Scalar(255, 169, 100)
+            ColorSpace.HSV,
+            new Scalar(20, 100, 177),
+            new Scalar(39, 255, 255)
     );
 
     public static final CustomColorRange GREEN = new CustomColorRange(
