894998e891f8bbe104038dd98be99597eba5a02f
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2024-12-18 14:22:10 +0200

    Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/registry_stuff/RegistryLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/registry_stuff/RegistryLinearOpMode.java
@@ -1,44 +1,23 @@
 package org.firstinspires.ftc.teamcode.registry_stuff;
 
-import static org.firstinspires.ftc.teamcode.registry_stuff.RegistryUtils.initCRServo;
 import static org.firstinspires.ftc.teamcode.registry_stuff.RegistryUtils.initMotor;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.enums.Colors;
-import org.firstinspires.ftc.teamcode.enums.Direction;
-import org.firstinspires.ftc.teamcode.enums.Stops;
-import org.firstinspires.ftc.teamcode.registry_stuff.registry_hardware.RegistryCRServo;
 import org.firstinspires.ftc.teamcode.registry_stuff.registry_hardware.RegistryDcMotor;
-import org.firstinspires.ftc.teamcode.registry_stuff.registry_hardware.RegistryDualMotor;
 
 // Abstract class that serves as a base for linear op modes with common robot components such as basic movement methods and it also used to give access to all
 // motors, sensors and servos in all subclasses without needing to copy them for each one and avoid repetitiveness
 public abstract class RegistryLinearOpMode extends LinearOpMode {
     // Declare motors, servos and sensors
     public static RegistryDcMotor encoderMotor;
-    public static RegistryDcMotor frontLeftDrive;
-    public static RegistryDcMotor frontRightDrive;
-    public static RegistryDcMotor rearLeftDrive;
-    public static RegistryDcMotor rearRightDrive;
-    public static RegistryDualMotor lifts;
-    public static RegistryDualMotor fronters;
-    public static RegistryCRServo rightServo;
-    public static RegistryCRServo leftServo;
     public static AnalogInput encoder;
-//    public static RegistrySmartServo servo;
     public static TouchSensor magnet;
-    //    public static RegistryDcMotor lift1;
-//    public static RegistryDcMotor lift2;
-//    public static RegistryDcMotor clawRotationCling;
-//    public static RegistryDcMotor rotationCling;
-//    public static RegistryServo clawRight;
-//    public static RegistryServo clawLeft;
 
     // Method used to call the initialization of all motors, sensors, and servos because the hardwareMap is set only when the Op Mode has began to run
     // it is also used to run the code provided by the subclasses (runCode)
@@ -50,9 +29,6 @@ public abstract class RegistryLinearOpMode extends LinearOpMode {
             waitForStart();
             while (opModeIsActive()) {
                 runCode();
-                if (this instanceof AutonomousRegistryLinearOpMode) {
-                    requestOpModeStop();
-                }
             }
         } else {
             runCode();
@@ -67,29 +43,10 @@ public abstract class RegistryLinearOpMode extends LinearOpMode {
     private void initConfig() {
         RegistryUtils.setMap(hardwareMap);
 
-        frontLeftDrive = initMotor("fl", DcMotorSimple.Direction.REVERSE);
-        rearLeftDrive = initMotor("rl", DcMotorSimple.Direction.REVERSE);
-        frontRightDrive = initMotor("fr", DcMotorSimple.Direction.FORWARD);
-        rearRightDrive = initMotor("rr", DcMotorSimple.Direction.FORWARD);
-        lifts = new RegistryDualMotor(hardwareMap, initMotor("l1", DcMotorSimple.Direction.REVERSE),
-                initMotor("l2"));
-        fronters = new RegistryDualMotor(hardwareMap, initMotor("f1", DcMotorSimple.Direction.REVERSE),
-                initMotor("f2"));
-        rightServo = initCRServo("rs", DcMotorSimple.Direction.FORWARD);
-        leftServo = initCRServo("ls", DcMotorSimple.Direction.REVERSE);
         encoder = hardwareMap.get(AnalogInput.class, "encoder");
-//        servo = initSmartServo("ss");
-//        servo = hardwareMap.get(CRServo.class, "ss");
-//        magnet = hardwareMap.get(TouchSensor.class, "ms");
-//        lifts.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        lift1 = getMotor("l1", DcMotorSimple.Direction.REVERSE);
-//        lift2 = getMotor("l2", DcMotorSimple.Direction.FORWARD);
-//        clawRight = getServo("cr", Servo.Direction.FORWARD);
-//        clawLeft = getServo("cl", Servo.Direction.REVERSE);
-//        rotationCling = getMotor("rcl", DcMotorSimple.Direction.FORWARD);
-//        clawRotationCling = getMotor("ccl", DcMotorSimple.Direction.REVERSE);
-        setMotorsZeroPowerBehaviour(Stops.WHEELS, DcMotor.ZeroPowerBehavior.BRAKE);
-        resetEncoders(Stops.ALL);
+        encoderMotor = initMotor("tm");
+
+//        resetEncoder(encoderMotor);
     }
 
      public static void setGamepadColor(Gamepad gamepad, Colors color, int duration) {
@@ -108,24 +65,10 @@ public abstract class RegistryLinearOpMode extends LinearOpMode {
         }
     }
 
-    // Method to reset encoders of specific motors (could be all of them)
-    public void resetEncoders(Stops stop) {
-        for (RegistryDcMotor motor : stop.motorList) {
-            resetEncoder(motor);
-        }
-    }
-
     // Method to reset the encoder of one specific motor
     public void resetEncoder(RegistryDcMotor motor) {
         motor.setMode(RegistryDcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(RegistryDcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
-    // Method to stop only certain motors (could be all of them)
-    public void stopOnly(Stops stop) {
-        for (RegistryDcMotor motor : stop.motorList) {
-            motor.setPower(0);
-        }
     }
 
     public void setMotorsModes(DcMotor.RunMode mode, RegistryDcMotor... motors) {
@@ -133,97 +76,4 @@ public abstract class RegistryLinearOpMode extends LinearOpMode {
             motor.setMode(mode);
         }
     }
-
-    public void setMotorsModes(Stops stop, DcMotor.RunMode mode) {
-        for (RegistryDcMotor motor : stop.motorList) {
-            motor.setMode(mode);
-        }
-    }
-
-    public void setMotorsZeroPowerBehaviour(Stops stop, DcMotor.ZeroPowerBehavior powerBehavior) {
-        for (RegistryDcMotor motor : stop.motorList) {
-            motor.setZeroPowerBehavior(powerBehavior);
-        }
-    }
-
-    public static void runMotors(double frontLeft, double frontRight, double rearLeft, double rearRight) {
-        frontLeftDrive.setPower(frontLeft);
-        frontRightDrive.setPower(frontRight);
-        rearLeftDrive.setPower(rearLeft);
-        rearRightDrive.setPower(rearRight);
-    }
-
-    // Method to move the robot in a specific direction
-    public void moveDirection(Direction direction, double speed) {
-        switch (direction) {
-            case FORWARD:
-                for (RegistryDcMotor motor : Stops.WHEELS.motorList) {
-                    motor.setPower(speed);
-                }
-                break;
-            case BACKWARD:
-                for (RegistryDcMotor motor : Stops.WHEELS.motorList) {
-                    motor.setPower(-speed);
-                }
-                break;
-            case LEFT:
-                for (RegistryDcMotor motor : Stops.WHEELS.motorList) {
-                    if (motor == frontLeftDrive || motor == rearRightDrive) {
-                        motor.setPower(speed);
-                    } else {
-                        motor.setPower(-speed);
-                    }
-                }
-                break;
-            case RIGHT:
-                for (RegistryDcMotor motor : Stops.WHEELS.motorList) {
-                    if (motor == frontRightDrive || motor == rearLeftDrive) {
-                        motor.setPower(speed);
-                    } else {
-                        motor.setPower(-speed);
-                    }
-                }
-                break;
-        }
-    }
-
-//    void moveLifts(double speed) {
-//        for (DcMotor motor : Stops.LIFTS.motorList) {
-//            motor.setPower(speed);
-//        }
-//    }
-//
-//    public void liftsUp(float speed) {
-//        if (!(Math.abs(lift1.getCurrentPosition()) >= LIFT_MAX_POSITION) || !(Math.abs(lift2.getCurrentPosition()) >= LIFT_MAX_POSITION)) {
-//            moveLifts(speed);
-//        }
-//    }
-//
-//    public void liftsDown(float speed) {
-//        moveLifts(-speed);
-//    }
-
-//    public void clingsUp(float speed) {
-//        moveClings(speed);
-//    }
-//
-//    public void clingsDown(float speed) {
-//        moveClings(-speed);
-//    }
-
-//    public void moveClings(float speed) {
-//        rotationCling.setPower(speed);
-//    }
-
-//    public void closeClaws() {
-//        setClaw(clawLeft, ClawState.CLOSE);
-//        setClaw(clawRight, ClawState.CLOSE);
-//    }
-//    public void openClaws() {
-//        setClaw(clawLeft, ClawState.OPEN);
-//        setClaw(clawRight, ClawState.OPEN);
-//    }
-//    public void setClaw(Servo claw, ClawState clawState) {
-//        claw.setPosition(clawState == ClawState.OPEN ? 0 : 0.5);
-//    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThroughBoreEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ThroughBoreEncoderTest.java
@@ -3,19 +3,67 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.registry_stuff.RegistryLinearOpMode;
+import org.firstinspires.ftc.teamcode.teleop.button.PressAndReleaseButton;
 
 @TeleOp(name = "Through Bore Encoder Test", group = "TeleOp")
 public class ThroughBoreEncoderTest extends RegistryLinearOpMode {
+    // Encoder Through Voltage (doesn't work right): encoder.getVoltage() / encoder.getMaxVoltage() * 360;
 
-    public static int MIN_ANGLE = 0;
-    public static int MAX_ANGLE = 360;
+    public static final double ERROR_SPEED = 0.05;
+    public static final int REVOLUTION_MARGIN = 8192 / 360;
+    private static final double SPEED = 0.5;
+    private static final int MAX_RIGHT_DEGREES = -180;
+    private static final int MAX_LEFT_DEGREES = 180;
+    private static final int ERROR_RANGE = 150;
+
+    private final PressAndReleaseButton triangle = new PressAndReleaseButton();
+    private final PressAndReleaseButton square = new PressAndReleaseButton();
+    private final PressAndReleaseButton circle = new PressAndReleaseButton();
+
+    private int targetAngle = 0;
 
     @Override
     public void runCode() {
-        int encoderTroughMotor = encoderMotor.getCurrentPosition();
-        double encoderTroughAnalog = encoder.getVoltage() / encoder.getMaxVoltage() * 360;
-//        telemetry.addData("Rotations", );
+        square.tick(gamepad1.square, () -> targetAngle++);
+        circle.tick(gamepad1.circle, () -> targetAngle--);
+        triangle.tick(gamepad1.triangle, () -> {
+            moveToAngle(targetAngle);
+        });
+
+        int encoderThroughMotor = getScaledAngle(encoderMotor.getCurrentPosition());
+        telemetry.addData("Rotations", encoderThroughMotor);
+        telemetry.addData("Target Angle", targetAngle);
         telemetry.update();
     }
-}
 
+    private void moveToAngle(int desiredAngle) {
+        int currentAngle = getScaledAngle(encoderMotor.getCurrentPosition());
+        if (currentAngle == desiredAngle) {
+            encoderMotor.setPower(0);
+            return;
+        }
+
+        int negative = desiredAngle < 0 ? -1 : 1;
+        boolean shouldMoveRight = currentAngle < desiredAngle;
+        boolean shouldMoveLeft = currentAngle > desiredAngle;
+
+        if (negative == 1 ? shouldMoveRight : shouldMoveLeft) {
+            encoderMotor.setPower(SPEED * negative);
+        } else {
+            encoderMotor.setPower((Math.abs(currentAngle) - Math.abs(desiredAngle) > ERROR_RANGE ? -SPEED : -ERROR_SPEED) * negative);
+        }
+    }
+
+    // doesn't work for new claw ):
+    // i did math for noting
+    private static int getSwerveAngle(int currentAngle, int desiredAngle) {
+        int directAngle = (desiredAngle - currentAngle + 180) % 360 - 180;
+        int reverseAngle = (desiredAngle - currentAngle + 360) % 360 - 180;
+        return currentAngle + Math.abs(directAngle) <= Math.abs(reverseAngle) ? directAngle : reverseAngle;
+    }
+
+    private static int getScaledAngle(int angle) {
+        int actualDegrees = angle / REVOLUTION_MARGIN;
+        return actualDegrees == MAX_LEFT_DEGREES ? MAX_LEFT_DEGREES : actualDegrees % MAX_LEFT_DEGREES * (actualDegrees > MAX_LEFT_DEGREES ? -1 : 1);
+    }
+}
